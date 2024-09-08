c9fa5565d8fec26f1f0fde6e3227d2be163e4e8a
melWiss <melWiss@users.noreply.github.com>
2024-09-08 16:14:05 +0000

    deploy: 8f42d4111901a734f779eafe429f2bf646da2dc9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "355609"
+    serviceWorkerVersion: "2005608028"
   }
 });
