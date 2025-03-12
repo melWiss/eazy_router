75dd783bb2033dceb499af957878e6651d31c8d2
melWiss <melWiss@users.noreply.github.com>
2025-03-12 01:40:33 +0000

    deploy: 41072ec49413fa45ede4f38acbd0a9b0c92b3916

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "manifest.json": "b0f5fe5cb6d5a4e820ed888184eb340a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.wasm": "5422aa3bceb31575b0db1554d002225b",
-"flutter_bootstrap.js": "ed90c4221006a2bc7c2006a3f4144a4c"};
+"flutter_bootstrap.js": "04cea499dbb6bee3acc6164b4a11fbc4"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
