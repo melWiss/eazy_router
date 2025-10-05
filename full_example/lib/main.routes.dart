020ac2aabde67decfdb86417a9792887d967d0f5
Oussama Maatallah <oussama.linfao.97@gmail.com>
2025-10-05 16:01:11 +0100

    updated the example

--- full_example/lib/main.routes.dart
+++ full_example/lib/main.routes.dart
@@ -1,3 +1,4 @@
+// dart format width=80
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
@@ -5,24 +6,39 @@
 // **************************************************************************
 
 import 'package:eazy_router/eazy_router.dart';
-import 'package:full_example/src/not_found_page.dart';
-import 'package:full_example/src/admin_page.dart';
+import 'package:full_example/src/second.dart';
+import 'package:full_example/src/bottom_sheet.dart';
+import 'package:full_example/src/processing_dialog.dart';
 import 'package:full_example/src/third.dart';
+import 'package:full_example/src/admin_page.dart';
+import 'package:full_example/src/anonymous_bottom_sheet.dart';
 import 'package:full_example/src/home_body/nested_home_body.dart';
 import 'package:full_example/src/home_body/non_nested_home_body.dart';
-import 'package:full_example/src/bottom_sheet.dart';
-import 'package:full_example/src/second.dart';
-import 'package:full_example/src/anonymous_bottom_sheet.dart';
-import 'package:full_example/src/processing_dialog.dart';
+import 'package:full_example/src/not_found_page.dart';
 import 'package:full_example/src/home.dart';
 
 void registerRoutes() {
   EazyRouterConfiguration.instance.router.registerRoutes(
     {
-      'not-found': (params) => NotFoundPageRoute.fromQueryParam(params),
+      'second': (params) => SecondScaffoldRoute.fromQueryParam(params),
     },
   );
-  EazyRouterConfiguration.instance.router.setNotFoundRoute(NotFoundPageRoute());
+  EazyRouterConfiguration.instance.router.registerRoutes(
+    {
+      'bottom-sheet-example': (params) =>
+          BottomSheetExampleRoute.fromQueryParam(params),
+    },
+  );
+  EazyRouterConfiguration.instance.router.registerRoutes(
+    {
+      'processing': (params) => ProcessingDialogRoute.fromQueryParam(params),
+    },
+  );
+  EazyRouterConfiguration.instance.router.registerRoutes(
+    {
+      'third': (params) => ThirdScaffoldRoute.fromQueryParam(params),
+    },
+  );
   EazyRouterConfiguration.instance.router.registerRoutes(
     {
       'admin': (params) => AdminPageRoute.fromQueryParam(params),
@@ -30,7 +46,8 @@ void registerRoutes() {
   );
   EazyRouterConfiguration.instance.router.registerRoutes(
     {
-      'third': (params) => ThirdScaffoldRoute.fromQueryParam(params),
+      'anonymous-bottom-sheet': (params) =>
+          AnonymousBottomSheetRoute.fromQueryParam(params),
     },
   );
   EazyRouterConfiguration.instance.router.registerRoutes(
@@ -45,26 +62,10 @@ void registerRoutes() {
   );
   EazyRouterConfiguration.instance.router.registerRoutes(
     {
-      'bottom-sheet-example': (params) =>
-          BottomSheetExampleRoute.fromQueryParam(params),
+      'not-found': (params) => NotFoundPageRoute.fromQueryParam(params),
     },
   );
-  EazyRouterConfiguration.instance.router.registerRoutes(
-    {
-      'second': (params) => SecondScaffoldRoute.fromQueryParam(params),
-    },
-  );
-  EazyRouterConfiguration.instance.router.registerRoutes(
-    {
-      'anonymous-bottom-sheet': (params) =>
-          AnonymousBottomSheetRoute.fromQueryParam(params),
-    },
-  );
-  EazyRouterConfiguration.instance.router.registerRoutes(
-    {
-      'processing': (params) => ProcessingDialogRoute.fromQueryParam(params),
-    },
-  );
+  EazyRouterConfiguration.instance.router.setNotFoundRoute(NotFoundPageRoute());
   EazyRouterConfiguration.instance.router.registerRoutes(
     {
       'home': (params) => HomeScaffoldRoute.fromQueryParam(params),
